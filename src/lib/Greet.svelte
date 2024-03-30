aab37adbf1fc42da3953759e7d628028723cff05
sklbz <sokolbozanic@gmail.com>
2024-03-30 21:46:26 +0100

    init

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { invoke } from "@tauri-apps/api/tauri"
+  import { listen } from "@tauri-apps/api/event"
+
+  listen("event_name")
+
+  let name = "";
+  let greetMsg = ""
+
+  async function greet(){
+    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+    greetMsg = await invoke("greet", { name })
+  }
+</script>
+
+<div>
+  <form class="row" on:submit|preventDefault={greet}>
+    <input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+    <button type="submit">Greet</button>
+  </form>
+  <p>{greetMsg}</p>
+</div>
