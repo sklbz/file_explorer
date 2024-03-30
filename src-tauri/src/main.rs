c8baa99c42f83a6aa6c5649ef21348f6d2ae4cb3
sklbz <sokolbozanic@gmail.com>
2024-03-30 22:05:41 +0100

    .

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,7 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use tauri::{AppHandle, CustomMenuItem, Manager, Menu,  MenuItem, Submenu};
-
-let quit: CustomMenuItem = CustomMenuItem::new("quit".to_string(), "Quit").unwrap();
-let close: CustomMenuItem = CustomMenuItem::new("close".to_string(), "Close").unwrap();
-let submenu: Submenu = Submenu::new("File", Menu::new().add_item(quit).add_item(close)).unwrap();
-let menu: Menu = Menu::new()
-    .add_native_item(MenuItem::Copy)
-    .add_item(CustomMenuItem::new("hide", "Hide"))
-    .add_submenu(submenu)
-    .unwrap();
+use tauri::{AppHandle, Manager};
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
